5d3f881c69f0ab96e5a7ac22b079c43f6589b855
Erfanffa <erfanffa2020@gmail.com>
2025-11-14 13:54:02 +0330

    refactoring: rename api response fields

--- Application/Utils/ApiResponse.cs
+++ Application/Utils/ApiResponse.cs
@@ -6,31 +6,41 @@ public class ApiResponse<T>
 {
     public T Data { get; set; }
     public HttpStatusCode StatusCode { get; set; }
-    public string Code { get; set; }
+    public string Message { get; set; }
 
     public static ApiResponse<T> Ok(T data)
     {
         return new ApiResponse<T> { Data = data, StatusCode = HttpStatusCode.OK };
     }
 
-    public static ApiResponse<T> Error(string code, HttpStatusCode statusCode)
+    public static ApiResponse<T> Error(string message, HttpStatusCode statusCode)
     {
-        return new ApiResponse<T> { Code = code, StatusCode = statusCode };
+        return new ApiResponse<T> { Message = message, StatusCode = statusCode };
+    }
+    
+    public static ApiResponse<T> BadRequest(string message)
+    {
+        return new ApiResponse<T> { Message = message, StatusCode = HttpStatusCode.BadRequest };
     }
 }
 
 public class ApiResponse
 {
     public HttpStatusCode StatusCode { get; set; }
-    public string Code { get; set; }
+    public string Message { get; set; }
 
     public static ApiResponse Ok()
     {
         return new ApiResponse{StatusCode = HttpStatusCode.OK };
     }
-
-    public static ApiResponse Error(string code, HttpStatusCode statusCode)
+    
+    public static ApiResponse BadRequest(string message)
     {
-        return new ApiResponse { Code = code, StatusCode = statusCode };
+        return new ApiResponse { Message = message, StatusCode = HttpStatusCode.BadRequest };
+    }
+
+    public static ApiResponse Error(string message, HttpStatusCode statusCode)
+    {
+        return new ApiResponse { Message = message, StatusCode = statusCode };
     }
 }
