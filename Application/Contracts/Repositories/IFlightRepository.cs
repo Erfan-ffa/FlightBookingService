d80d1369145c74f7abe66feb3b88ab49bdd586c2
Erfanffa <erfanffa2020@gmail.com>
2025-11-15 23:29:37 +0330

    feat: add get flight with row-level lock method

--- Application/Contracts/Repositories/IFlightRepository.cs
+++ Application/Contracts/Repositories/IFlightRepository.cs
@@ -9,4 +9,8 @@ public interface IFlightRepository
     Task<bool> ExistsAsync(string flightNumber, CancellationToken cancellationToken);
     Task<List<Flight>> GetAllAsync(string origin, string destination, DateOnly departureDate,
         CancellationToken cancellationToken);
+
+    Task<Flight?> GetByIdAsync(long id, CancellationToken cancellationToken);
+    Task<int> DecreaseAvailableSeatsAsync(long flightId, CancellationToken cancellationToken);
+    Task<Flight?> GetWithLockAsync(long id, CancellationToken cancellationToken);
 }
