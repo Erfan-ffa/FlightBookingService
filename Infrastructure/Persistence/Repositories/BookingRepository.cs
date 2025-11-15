1d90e492490461089fc375b309efb0882b85ea24
Erfanffa <erfanffa2020@gmail.com>
2025-11-15 23:26:06 +0330

    feat: add booked seats method

--- Infrastructure/Persistence/Repositories/BookingRepository.cs
+++ Infrastructure/Persistence/Repositories/BookingRepository.cs
@@ -6,8 +6,24 @@ namespace Infrastructure.Persistence.Repositories;
 
 public class BookingRepository(AppDbContext dbContext) : IBookingRepository
 {
+    public void Add(Booking booking)
+    {
+        dbContext.Bookings.Add(booking);
+    }
 
     public async Task<List<Booking>> GetByFlightIdAsync(long id, CancellationToken cancellationToken)
         => await dbContext.Bookings.Include(x => x.Passenger)
             .Where(x => x.FlightId == id).ToListAsync(cancellationToken);
+
+    public HashSet<int> GetBookedSeatNumbers(long id, CancellationToken cancellationToken)
+    {
+        return dbContext.Bookings
+            .Where(x => x.Id == id)
+            .Select(x => x.SeatNumber)
+            .ToHashSet();
+    }
+
+    public async Task<bool> SeatAlreadyBooked(long flightId, int seatNumber, CancellationToken cancellationToken)
+        => await dbContext.Bookings.AnyAsync(x => x.FlightId == flightId && x.SeatNumber == seatNumber,
+            cancellationToken);
 }
