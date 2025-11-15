381acc5b9b4e5a37705c40fadc3cea9e74df7636
Erfanffa <erfanffa2020@gmail.com>
2025-11-15 23:26:16 +0330

    feat: add booked seats method

--- Application/Contracts/Repositories/IBookingRepository.cs
+++ Application/Contracts/Repositories/IBookingRepository.cs
@@ -5,4 +5,7 @@ namespace Application.Contracts.Repositories;
 public interface IBookingRepository
 {
     Task<List<Booking>> GetByFlightIdAsync(long id, CancellationToken cancellationToken);
+    HashSet<int> GetBookedSeatNumbers(long id, CancellationToken cancellationToken);
+    Task<bool> SeatAlreadyBooked(long flightId, int seatNumber, CancellationToken cancellationToken);
+    void Add(Booking booking);
 }
