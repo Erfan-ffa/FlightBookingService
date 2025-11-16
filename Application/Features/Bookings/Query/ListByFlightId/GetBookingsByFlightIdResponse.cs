4d51fba6aaf1a0568c62072ce6fcfa85f376dbdc
Erfanffa <erfanffa2020@gmail.com>
2025-11-16 22:14:04 +0330

    refactoring: rename bookings response field to meaningful name

--- Application/Features/Bookings/Query/ListByFlightId/GetBookingsByFlightIdResponse.cs
+++ Application/Features/Bookings/Query/ListByFlightId/GetBookingsByFlightIdResponse.cs
@@ -4,5 +4,5 @@ namespace Application.Features.Bookings.Query.ListByFlightId;
 
 public class GetBookingsByFlightIdResponse(List<BookingModel> bookings)
 {
-    public List<BookingModel> BookingModels { get; } = bookings;
+    public List<BookingModel> Bookings { get; } = bookings;
 }
